1450fe39aee7e3ad48aa1fddcacecd95341760ca
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-08-01 14:43:44 +0300

    New approach tested

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,17 @@
 const removeFromArray = function(array, value) {
-    return arr.filter(function(ele){ 
-            return ele != value; 
-        });
+
+    var arr = array;
+    
+    for( var i = 0; i < arr.length; i++){ 
+                                   
+        if ( arr[i] === value) { 
+            arr.splice(i, 1); 
+            i--; 
+        }
     }
+    return arr;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
