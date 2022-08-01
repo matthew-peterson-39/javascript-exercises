cda749749ddfb76b47658c49d108d42d9f6abce7
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-08-01 14:40:45 +0300

    Test method?

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, value) {
+    return arr.filter(function(ele){ 
+            return ele != value; 
+        });
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
