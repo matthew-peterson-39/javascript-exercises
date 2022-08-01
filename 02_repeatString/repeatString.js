1d087e2fccce8bbe204d5879b243bb836ca6b54f
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-08-01 14:01:15 +0300

    Fixed return statement and removed console.log()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,10 +3,8 @@ const repeatString = function(string, count) {
     for(let i = 0; i < count - 1; i++ ) {
         newString += string
     }
-    return console.log(newString);
+    return newString;
 };
-
-repeatString("hey", count = 3);
 
 // Do not edit below this line
 module.exports = repeatString;
