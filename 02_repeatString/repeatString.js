e6fe445d43e7b6867f46c22cea36d9a31fcaa09e
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-08-01 13:59:29 +0300

    First test of repeatSrting ready.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let newString = string;
+    for(let i = 0; i < count - 1; i++ ) {
+        newString += string
+    }
+    return console.log(newString);
 };
+
+repeatString("hey", count = 3);
 
 // Do not edit below this line
 module.exports = repeatString;
